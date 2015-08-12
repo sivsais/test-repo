848589fe75f028e15b8f869a84dd3c544e7a2a12
sivsais <sivsais@gmail.com>
2015-08-12 19:08:43 +0400

    main module

---
+++ src/mg-gui-helpers.js
@@ -0,0 +1,8 @@
+define(['helpers/mg-hint', 'helpers/mg-additional'],
+    function (hint, additional) {
+        return {
+            hint: hint,
+            additional: additional
+        }
+    }
+);
